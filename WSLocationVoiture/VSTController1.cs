72c2e66f555387217caaf598066c00ba4d01a877
YOUCEF <samy.spy00006@gmail.com>
2021-03-26 14:17:38 +0100

    second commit

---
+++ WSLocationVoiture/VSTController1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WSLocationVoiture
+{
+    public class VSTController1 : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<station_taxi> GetStationTaxi()
+        {
+            location_dbEntities entities = new location_dbEntities();
+            return entities.station_taxi;
+        }
+
+        public IEnumerable<voiture> GetAgenceLocation()
+        {
+            location_dbEntities entities = new location_dbEntities();
+            return entities.voitures;
+        }
+    }
+}
